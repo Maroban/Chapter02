a1f8b72239271c12d140ef0f4dcf4127b9b8ff2e
Maroban <dbtmdqja207@naver.com>
2021-06-01 19:23:53 +0900

    6/01 강의

---
+++ src/com/javaex/ex01/Good.java
@@ -0,0 +1,9 @@
+package com.javaex.ex01;
+
+public class Good {
+		
+	String name; //타입 필드명 => 이렇게 선언
+	int price;
+	
+
+}
---
+++ src/com/javaex/ex01/GoodsApp.java
@@ -0,0 +1,27 @@
+package com.javaex.ex01;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		//카메라
+		Good camera = new Good();
+		camera.name = "니콘"; //Goods.java 에 있는 틀을 이용해 지정해줄 수 있다.
+		camera.price = 400000;
+		System.out.println(camera.name + " , " + camera.price);
+
+		//컴퓨터
+		Good computer = new Good();
+		computer.name = "그램";
+		computer.price = 1000000;
+		System.out.println(computer.name + " , " + computer.price);
+		
+		//컵
+		Good cup = new Good();
+		cup.name = "머그컵";
+		cup.price = 2000;
+		System.out.println(cup.name + " , " + cup.price);
+		
+		
+	}
+
+}
